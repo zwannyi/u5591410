6372b82efe50df4448e40d44350cff4e9b1cfa4c
zwannyi <156784959+zwannyi@users.noreply.github.com>
2024-01-27 21:05:23 +0000

    Update speeding.py

--- speeding.py
+++ speeding.py
@@ -43,9 +43,4 @@ print(speeding_ticket(85, False))  # Big Ticket
 print(speeding_ticket(65, True))   # No Ticket
 print(speeding_ticket(85, True))   # Small Ticket
 
-# Test cases
-speeding_ticket(60, False)  # Expected output: "No Ticket"
-speeding_ticket(75, False)  # Expected output: "Small Ticket"
-speeding_ticket(85, False)  # Expected output: "Big Ticket"
-speeding_ticket(65, True)  # Expected output: "No Ticket"
-speeding_ticket(85, True)  # Expected output: "Small Ticket"
+
