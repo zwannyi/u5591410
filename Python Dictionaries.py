7653fcf91c9b9c4bd0f25c4a78d96eaa1b967754
zwannyi <156784959+zwannyi@users.noreply.github.com>
2024-01-29 20:54:48 +0000

    Add files via upload

---
+++ Python Dictionaries.py
@@ -0,0 +1,16 @@
+#Dictionaries are used to store data values in key:value pairs.
+#A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
+#Dictionaries are written with curly brackets{}, and have keys and values:
+thisdict =	{"brand": "Ford",
+            "model": "Mustang",
+            "year": 1964
+}
+print(thisdict)
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
