aeca31d7c8ed43a113eb123265993c84036d2582
zwannyi <156784959+zwannyi@users.noreply.github.com>
2024-02-02 22:30:52 +0000

    Update Python Dictionaries.py

--- Python Dictionaries.py
+++ Python Dictionaries.py
@@ -14,3 +14,38 @@ thisdict = {
   "year": 1964
 }
 print(thisdict["brand"])
+
+#Dictionaries cannot have two items with the same key:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict)
+
+#Print the number of items in the dictionary:
+print(len(thisdict))
+
+#String, int, boolean, and list data types:
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+
+#From Python's perspective, dictionaries are defined as objects with the data type 'dict':
+#Print the data type of a dictionary:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(type(thisdict))
+
+#It is also possible to use the dict() constructor to make a dictionary.
+#Using the dict() method to make a dictionary:
+
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
